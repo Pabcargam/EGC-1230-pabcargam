c75913a8b5df6e047dc0a6a625c84c1e4e76d72b
Pabcargam <pabcargam@alum.us.es>
2021-02-12 11:43:11 +0100

    Añadido los cambios para el funcionamiento de Heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -163,10 +163,13 @@ STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
+APIS = {}
 
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
+
+BASEURL = "https://egc-1230-pabcargam.herokuapp.com/"
 
 try:
     from local_settings import *
@@ -183,3 +186,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
